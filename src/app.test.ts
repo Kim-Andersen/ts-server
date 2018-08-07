c25616e35628479ec08f69b801ef94e7514e9302
kimandersen1@gmail.com <kimandersen1@gmail.com>
2018-08-07 18:58:41 +0200

    Initial commit

---
+++ src/app.test.ts
@@ -0,0 +1,11 @@
+import request from 'supertest';
+
+import app from '../src/app';
+
+describe('GET /random-url', () => {
+  it('should return 404', done => {
+    request(app)
+      .get('/random-url')
+      .expect(404, done);
+  });
+});
---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import errorHandler from 'errorHandler';
+
+import app from './app';
+
+// import errorHandler from 'errorhandler';
+
+/**
+ * Error Handler. Provides full stack - remove for production
+ */
+if (app.get('env') === 'development') {
+  app.use(errorHandler());
+}
+
+/**
+ * Start Express server.
+ */
+const server = app.listen(app.get('port'), () => {
+  console.log(
+    '  App is running at http://localhost:%d in %s mode',
+    app.get('port'),
+    app.get('env')
+  );
+  console.log('  Press CTRL-C to stop\n');
+});
+
+export default server;
