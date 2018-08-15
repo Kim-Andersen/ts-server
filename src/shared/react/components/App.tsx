3723b14710876caf6f0e083b6c32a50c07a2e7fc
kimandersen1@gmail.com <kimandersen1@gmail.com>
2018-08-15 20:09:09 +0200

    Trying form components

--- src/shared/react/components/App.tsx
+++ src/shared/react/components/App.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Dropdown, Input } from 'semantic-ui-react';
 
 export interface Props {
   email: string;
 }
+
+const friendOptions = [
+  {
+    text: 'Jenny Hess',
+    value: 'Jenny Hess',
+    image: { avatar: true, src: '/images/avatar/small/jenny.jpg' }
+  }
+];
 
 export default function App({ email }: Props) {
   return (
@@ -11,6 +19,24 @@ export default function App({ email }: Props) {
       <div className="greeting">Hello {email}</div>
 
       <Button>Click Here</Button>
+
+      <Input size="huge">
+        <Dropdown
+          size="huge"
+          placeholder="Select Friend"
+          fluid
+          selection
+          options={friendOptions}
+        />
+      </Input>
+
+      <Dropdown
+        size="huge"
+        placeholder="Select Friend"
+        fluid
+        selection
+        options={friendOptions}
+      />
     </div>
   );
 }
